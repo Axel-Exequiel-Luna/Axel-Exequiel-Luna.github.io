17218e96226d6c7cd26d53ac23f9170bb6dc6fbe
Axel Exequiel Luna <exequielaxelluna@gmail.com>
2021-12-14 05:46:25 -0300

    Add files via upload
    first commit

---
+++ js/choice.js
@@ -0,0 +1,29 @@
+function clock(){
+          			
+    document.location.href='../index.html'
+
+};
+
+function cronometro(){
+
+    document.location.href='../html/main_chronometer.html'
+
+};
+
+function about(){
+
+    document.location.href='../html/main_about.html'
+
+};
+
+function clima(){
+
+document.location.href='../html/unavailable.html'
+
+};
+
+function no(){
+
+    document.location.href='../html/unavailable.html'
+
+};
